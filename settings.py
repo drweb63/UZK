43b43c7d5b9d9afc81b239f1ecb173adf93b474a
Andrey <drweb63@gmail.com>
2016-10-02 20:19:22 +0400

    добавлен файл зависимостей (requrements.txt) перенесены настройки базы данных в settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('postgresql+psycopg2://uzk:080166@localhost/uzk')
+session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+def init_db():
+    import models
+    Base.metadata.create_all(bind=engine)
+
 path_barcode = 'static/barcode/'
